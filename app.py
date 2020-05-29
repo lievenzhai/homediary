ec6265b1eb8894474bbc158cd31a625e85c078ec
lievenzhai <416662054@qq.com>
2020-05-29 21:34:26 +0800

    add database

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from flask import Flask, url_for, render_template
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+import os, sys
 
+app = Flask(__name__)
+# 写入了一个 SQLALCHEMY_DATABASE_URI 变量来告诉 SQLAlchemy 数据库连接地址
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATTONS'] = False  # 关闭对模型修改的监控
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):  # 表名将会是user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+
+class Diary(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(40))
+    article = db.Column(db.String(4000))
+
+# 定义时间
 now = datetime.now()
 
 # 虚拟数据
@@ -12,11 +33,11 @@ diaries1 = [
 ]
 
 
-app = Flask(__name__)
-
 @app.route('/')
 def index():
-    return render_template('index.html', name=name1, diaries=diaries1)
+    user = User.query.first()
+    diarys = Diary.query.all()
+    return render_template('index.html', user=user, diaries=diarys)
 
 app.run()
 

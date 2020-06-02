f1c569ce6904f079d590d59577fb53bb9df010e6
lievenzhai <416662054@qq.com>
2020-06-02 17:12:50 +0800

    添加编辑和删除

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request, flash, redirect
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 import os, sys
@@ -9,6 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_pat
 app.config['SQLALCHEMY_TRACK_MODIFICATTONS'] = False  # 关闭对模型修改的监控
 
 db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'dev'
 
 
 class User(db.Model):  # 表名将会是user（自动生成，小写处理）
@@ -18,7 +19,8 @@ class User(db.Model):  # 表名将会是user（自动生成，小写处理）
 
 class Diary(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(40))
+    # user = db.Column(db.String(20))
+    title = db.Column(db.String(60))
     article = db.Column(db.String(4000))
 
 # 定义时间
@@ -41,12 +43,60 @@ def inject_user():
 
 @app.route('/')
 def index():
-    diarys = Diary.query.all()
-    return render_template('index.html', diaries=diarys)
+    diaries = Diary.query.all()
+    return render_template('index.html', diaries=diaries)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+
+@app.route('/diary', methods=['GET', 'POST'])
+def diary():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        article = request.form.get('article')
+        if not title or len(title) > 60:
+            flash('无效的输入')
+            return redirect(url_for('diary'))  # ??
+        diary = Diary(title=title, article=article)
+        db.session.add(diary)
+        db.session.commit()
+        flash('新的日记已保存')
+        return redirect(url_for('index'))
+    diaries = Diary.query.all()
+    return render_template('diary.html',diaries=diaries)
+
+
+@app.route('/diary/edit<int:diary_id>', methods=['GET', 'POST'])
+def edit(diary_id):
+    diary = Diary.query.get_or_404(diary_id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        article = request.form['article']
+        if not title or not article or len(title) > 60 or len(article) > 4000:
+            flash('错误输入')
+            return redirect(url_for('edit', diary_id=diary_id))
+
+        diary.title = title
+        diary.article = article
+        db.session.commit()
+        flash('编辑更新完成')
+        return redirect(url_for('index'))
+    return render_template('edit.html', diary=diary)
+
+
+@app.route('/diary/delete/int:<diary_id>', methods=['POST'])
+def delete(diary_id):
+    diary = Diary.query.get_or_404(diary_id)
+    db.session.delete(diary)
+    db.session.commit()
+    flash('删除成功.')
+    return redirect(url_for('index'))
+
+
 
 app.run()
 
